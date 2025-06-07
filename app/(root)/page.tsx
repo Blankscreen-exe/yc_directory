fe1c4ee71d1a1d8ca8db9fc909aecf657ffb0d6a
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2025-06-07 14:36:45 +0500

    add hero component custom styling

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1 className="heading">Home</h1>  
+    <section className='hero_container'>
+      <div className="hero_inner_container"></div>
+      
+      <h1 className="heading">Pitch Your Idea</h1>  
+      <p className="sub_heading !max-w-3xl">
+        Submit your ideas and get noticed
+      </p>
+    </section>
     </>
   );
 }
