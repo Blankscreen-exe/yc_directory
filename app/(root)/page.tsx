b2f4ca6b982954f67a05c65faf2e7fa49d45f7d3
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2025-06-07 17:53:51 +0500

    feat(search): add search bar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,10 @@
 import Image from "next/image";
+import SearchForm from "@/app/components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}: {
+  searchParams: Promise<{query?: string}>
+}) {
+  const query = (await searchParams).query;
   return (
     <>
     <section className='hero_container relative'>
@@ -11,6 +15,7 @@ export default function Home() {
         <p className="sub-heading bottom-20 !max-w-3xl">
           Submit your ideas and get noticed
         </p>
+        <SearchForm query={query}/>
       </div>
     </section>
     </>
