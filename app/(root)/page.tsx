543d567823899c75253ac21f2e6465124840f337
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2025-06-07 17:08:52 +0500

    fix(hero section):content alignment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <section className='hero_container'>
+    <section className='hero_container relative'>
       <div className="hero_inner_container"></div>
       
-      <h1 className="heading">Pitch Your Idea</h1>  
-      <p className="sub_heading !max-w-3xl">
-        Submit your ideas and get noticed
-      </p>
+      <div className="hero-content">
+        <h1 className="heading ">Pitch Your Idea</h1>  
+        <p className="sub-heading bottom-20 !max-w-3xl">
+          Submit your ideas and get noticed
+        </p>
+      </div>
     </section>
     </>
   );
