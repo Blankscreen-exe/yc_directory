640ed7e30ced11200e5e997c3d954d56bc1e2c0f
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2025-06-07 18:02:13 +0500

    feat(search): add current search status

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,17 +7,24 @@ export default async function Home({searchParams}: {
   const query = (await searchParams).query;
   return (
     <>
-    <section className='hero_container relative'>
-      <div className="hero_inner_container"></div>
-      
-      <div className="hero-content">
-        <h1 className="heading ">Pitch Your Idea</h1>  
-        <p className="sub-heading bottom-20 !max-w-3xl">
-          Submit your ideas and get noticed
+      <section className='hero_container relative'>
+        <div className="hero_inner_container"></div>
+        
+        <div className="hero-content">
+          <h1 className="heading ">Pitch Your Idea</h1>  
+          <p className="sub-heading bottom-20 !max-w-3xl">
+            Submit your ideas and get noticed
+          </p>
+          <SearchForm query={query}/>
+        </div>
+      </section>
+
+      <section className="section_container">
+        <p className='text-30-semibold'>
+          {query ? `Search results for: "${query}"` : 'All Startups'}
         </p>
-        <SearchForm query={query}/>
-      </div>
-    </section>
+      </section>
+      
     </>
   );
 }
